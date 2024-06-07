94ac19fb8bc36c03d6f41c979cd97ea9ccef00eb
xiatianrui1110 <xiatianrui1110@gmail.com>
2024-06-07 13:12:52 +0100

    rebase evaluation commits

---
+++ evaluation/scripts/cli.py
@@ -0,0 +1,105 @@
+import asyncio
+import logging
+from pathlib import Path
+
+import dotenv
+import typer
+from rich.logging import RichHandler
+
+from . import service_setup
+from .evaluate import run_evaluate_from_config
+from .generate import generate_test_qa_answer, generate_test_qa_data
+from .red_teaming import run_red_teaming
+
+app = typer.Typer(pretty_exceptions_enable=False)
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
+logger = logging.getLogger("scripts")
+
+logger.setLevel(logging.INFO)
+
+dotenv.load_dotenv(override=True)
+
+
+def int_or_none(raw: str) -> Optional[int]:
+    return None if raw == "None" else int(raw)
+
+
+def str_or_none(raw: str) -> Optional[str]:
+    return None if raw == "None" else raw
+
+
+@app.command()
+def evaluate(
+    config: Path = typer.Option(
+        exists=True,
+        dir_okay=False,
+        file_okay=True,
+        help="Path to config.json",
+        default="config.json",
+    ),
+    numquestions: Optional[int] = typer.Option(
+        help="Number of questions to evaluate (defaults to all if not specified).",
+        default=None,
+        parser=int_or_none,
+    ),
+    targeturl: Optional[str] = typer.Option(
+        help="URL of the target service to evaluate against (defaults to the one in the config).",
+        default=None,
+        parser=str_or_none,
+    ),
+):
+    run_evaluate_from_config(Path.cwd(), config, numquestions, targeturl)
+
+
+@app.command()
+def generate(
+    output: Path = typer.Option(exists=False, dir_okay=False, file_okay=True),
+    numquestions: int = typer.Option(help="Number of questions to generate", default=200),
+    persource: int = typer.Option(help="Number of questions to generate per source", default=5),
+):
+    generate_test_qa_data(
+        openai_config=service_setup.get_openai_config_dict(),
+        search_client=service_setup.get_search_client(),
+        num_questions_total=numquestions,
+        num_questions_per_source=persource,
+        output_file=Path.cwd() / output,
+    )
+
+
+@app.command()
+def generate_answers(
+    input: Path = typer.Option(exists=True, dir_okay=False, file_okay=True),
+    output: Path = typer.Option(exists=False, dir_okay=False, file_okay=True),
+):
+    generate_test_qa_answer(
+        openai_config=service_setup.get_openai_config(),
+        question_path=Path.cwd() / input,
+        output_file=Path.cwd() / output,
+    )
+
+
+@app.command()
+def red_teaming(
+    scorer_path: Path = typer.Option(
+        exists=True,
+        dir_okay=False,
+        file_okay=True,
+        default="scorer_definitions/key_logger_classifier.yaml",
+    ),
+    red_teaming_llm: Optional[str] = typer.Option(default="openai"),
+    prompt_target: Optional[str] = typer.Option(default="openai"),
+):
+    red_team = service_setup.get_openai_target() if red_teaming_llm == "openai" else service_setup.get_azure_ml_target()
+    target = service_setup.get_openai_target() if prompt_target == "openai" else service_setup.get_azure_ml_target()
+
+    asyncio.run(run_red_teaming(Path.cwd() / scorer_path, red_team, target))
+
+
+def cli():
+    app()
---
+++ evaluation/scripts/generate.py
@@ -0,0 +1,97 @@
+import json
+import logging
+from pathlib import Path
+
+from azure.ai.generative.synthetic.qa import QADataGenerator, QAType
+from azure.search.documents import SearchClient
+from openai_messages_token_helper import get_token_limit
+
+from . import service_setup
+
+logger = logging.getLogger("scripts")
+
+
+def generate_test_qa_data(
+    openai_config: dict,
+    search_client: SearchClient,
+    num_questions_total: int,
+    num_questions_per_source: int,
+    output_file: Path,
+):
+    logger.info(
+        "Generating %d questions total, %d per source, based on search results",
+        num_questions_total,
+        num_questions_per_source,
+    )
+
+    qa_generator = QADataGenerator(model_config=openai_config)
+
+    r = search_client.search("", top=1000)
+    qa: list[dict] = []
+    for doc in r:
+        if len(qa) > num_questions_total:
+            break
+        logger.info("Processing search document %s", doc["sourcepage"])
+        text = doc["content"]
+
+        result = qa_generator.generate(
+            text=text,
+            qa_type=QAType.LONG_ANSWER,
+            num_questions=num_questions_per_source,
+        )
+
+        for question, answer in result["question_answers"]:
+            citation = f"[{doc['sourcepage']}]"
+            qa.append({"question": question, "truth": answer + citation})
+
+    logger.info("Writing %d questions to %s", len(qa), output_file)
+    directory = Path(output_file).parent
+    if not directory.exists():
+        directory.mkdir(parents=True)
+    with open(output_file, "w", encoding="utf-8") as f:
+        for item in qa:
+            f.write(json.dumps(item) + "\n")
+
+
+def generate_test_qa_answer(
+    openai_config,
+    question_path: Path,
+    output_file: Path,
+    using_huggingface: bool = False,
+):
+    logger.info("Generating answers based on the quesion of %s", question_path)
+    with open(question_path, encoding="utf-8") as f:
+        questions = [json.loads(line) for line in f.readlines()]
+
+    if not using_huggingface:
+        logger.info("Using Azure OpenAI Service")
+        openai_client = service_setup.get_openai_client(openai_config)
+
+        for question in questions:
+            response = openai_client.chat.completions.create(
+                model=openai_config.model,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": f"{question['question']}",
+                    }
+                ],
+                n=1,
+                max_tokens=get_token_limit(openai_config.model),
+                temperature=0.3,
+            )
+            answer = response.choices[0].message.content.split("\n")[0]
+            print(answer)
+            question["answer"] = answer
+    else:
+        # TODO: Implement Hugging Face Service
+        logger.info("Using Hugging Face Service")
+
+    logger.info("Writing %d questions with answer to %s", len(questions), output_file)
+
+    directory = Path(output_file).parent
+    if not directory.exists():
+        directory.mkdir(parents=True)
+    with open(output_file, "w", encoding="utf-8") as f:
+        for item in questions:
+            f.write(json.dumps(item) + "\n")
