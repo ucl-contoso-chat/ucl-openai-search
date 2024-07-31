fce53ce520f3b542027da80c2d6560e43ce20a9f
Phoevos Kalemkeris <phoevos@protonmail.com>
2024-07-31 15:22:04 +0000

    feat: Add util for saving JSONL files
    Signed-off-by: Phoevos Kalemkeris <phoevos@protonmail.com>

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -12,3 +12,11 @@ def load_jsonl(path: Path) -> list[dict]:
     """Load a JSONL file."""
     with open(path, encoding="utf-8") as f:
         return [json.loads(line) for line in f.readlines()]
+
+
+def save_jsonl(data: list[dict], path: Path):
+    """Save a list of dictionaries to a JSONL file."""
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, "w", encoding="utf-8") as f:
+        for item in data:
+            f.write(json.dumps(item) + "\n")
