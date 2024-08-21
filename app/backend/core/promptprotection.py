ce20994af80111191fcd68dc27a932147a2b0430
Phoevos Kalemkeris <phoevos@protonmail.com>
2024-08-21 10:03:36 +0000

    fix: Minor docstring changes
    Fixed docstrings in promptprotection.py, slightly rephrasing some for
    clarity and making sure that they're in proper markdown so that they
    are rendered correctly in the documentation.
    Renamed the 'config' dictionary of the 'PromptProtection' class to
    'protections'.
    
    Signed-off-by: Phoevos Kalemkeris <phoevos@protonmail.com>

--- app/backend/core/promptprotection.py
+++ app/backend/core/promptprotection.py
@@ -4,31 +4,34 @@ from typing import Dict
 
 @dataclass
 class ProtectionMechanism:
-    """Abstract method to check for a violation.
-    This method should be implemented by subclasses to define a logic for specific protection mechanism,
-    checking for a violation in the input message.
+    """Base class for a protection mechanism to detect a violation.
 
-    Args:
+    Classes adding support for a specific protection mechanism should inherit from this class,
+    implementing the `check_for_violation` method to detect issues in the input message.
+
+    Attributes:
         model_name (str): The name of the model to use for the protection mechanism.
         enabled (bool): Whether the protection mechanism is enabled or not.
-
-    Returns:
-        bool: False if the message does not violate the protection mechanism, True otherwise.
     """
 
     model_name: str
     enabled: bool = False
 
     async def check_for_violation(self, **kwargs) -> bool:
+        """Check for a violation in the input message using a protection mechanism.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments that are accepted by the specific protection mechanism.
+                The implementation should ensure that all required arguments are provided.
+
+        Returns:
+            bool: False if the protection mechanism detects a violation (check failed), True otherwise (check succeeded).
+        """
         raise NotImplementedError
 
     @staticmethod
     def run_if_enabled(func):
-        """Decorator to run the function only if the 'enabled' attribute is True.
-
-        Returns:
-            Callable: the wrapped function only running if the protection mechanism is enabled.
-        """
+        """Decorator to run a method only if the `enabled` attribute is True for the protection mechanism."""
 
         async def wrapper(self, *args, **kwargs):
             if self.enabled:
@@ -41,29 +44,28 @@ class ProtectionMechanism:
 @dataclass
 class InjectionProtection(ProtectionMechanism):
     """Protection mechanism for detecting prompt injection attacks.
-    All the protection mechanisms are supposed to be implemented as subclasses of ProtectionMechanism.
 
     Attributes:
         model_name (str): The name of the model to use for the protection mechanism.
-        enabled (bool): Whether to use this protection mechanism or not.
+        enabled (bool): Whether the protection mechanism is enabled or not.
     """
 
     model_name: str = "protectai/deberta-v3-base-prompt-injection"
 
     @ProtectionMechanism.run_if_enabled
     async def check_for_violation(self, **kwargs) -> bool:
-        """Check for prompt injection attacks in the input message using the text specification LLM.
+        """Check for prompt injection attacks in the input message using a text classification LLM.
 
         Args:
-            **kwargs: Arbitrary keyword arguments. For running text_classification, must include 'llm_client' and 'message'.
+            **kwargs: Arbitrary keyword arguments. For running `text_classification`,
+                `llm_client` and `message` must be included.
 
         Returns:
-            bool: False if the message does contain a prompt injection, True otherwise.
+            bool: False if the message contains a prompt injection (check failed), True otherwise (check succeeded).
 
         Raises:
-            ValueError: If the required arguments ('llm_client' and 'message') are not provided.
+            ValueError: If the required arguments (`llm_client` and `message`) are not provided.
         """
-
         required_args = frozenset(["llm_client", "message"])
         if not required_args.issubset(kwargs):
             raise ValueError(f"Missing required arguments: {required_args - kwargs.keys()}")
@@ -78,48 +80,54 @@ class InjectionProtection(ProtectionMechanism):
 
 class PromptProtection:
     """Class to manage various prompt protection mechanisms.
-    To add new protection mechanisms, first the ProtectonMechanism class should be implemented.
-    Then, the new class should be added to the config inside of the __init__ method.
+
+    To add new protection mechanisms, create a new class inheriting from `ProtectonMechanism` and
+    implement the respective `check_for_violation` method. Then, the new class should be added to
+    the dict of registered protection mechanisms (i.e. `protections`) defined in the `__init__` method.
 
     Attributes:
-        config (Dict): A dictionary containing all the protection mechanisms.
+        protections (Dict[str, ProtectionMechanism]): A dictionary containing all registered protection mechanisms.
     """
 
-    def __init__(self, prompt_protection_enabled=False):
-        """Initialise the PromptProtection class with the given protection mechanisms, which are supposed
-        to be subclasses of ProtectionMechanism. Additionally, new arguments should be added to the constructor,
-        to initialise the protection mechanisms on startup (optional).
+    def __init__(self, injection_protection_enabled=False):
+        """Register and initialise the supported protection mechanisms.
+
+        Initialise the `PromptProtection` class with the supported protection mechanisms, which are
+        supposed to be subclasses of `ProtectionMechanism`. If new protection mechanisms are added,
+        new arguments can be added to the constructor to allow enabling them dynamically (optional).
 
         Args:
-            prompt_protection_enabled (bool): Enable or disable the injection protection mechanism.
+            injection_protection_enabled (bool): Enable or disable the injection protection mechanism.
         """
-        self.config: Dict = {
-            "injection_protection": InjectionProtection(enabled=prompt_protection_enabled),
+        self.protections: Dict[str, ProtectionMechanism] = {
+            "injection_protection": InjectionProtection(enabled=injection_protection_enabled),
         }
 
     def set_protection_bool(self, protection_name: str, value: bool):
-        """Set the 'enabled' attribute of the protection mechanism with the given name.
+        """Set the `enabled` attribute of the protection mechanism with the given name.
 
         Args:
             protection_name (str): The name of the protection mechanism to enable or disable.
             value (bool): Whether to enable or disable the protection mechanism.
         """
-        if protection_name in self.config:
-            self.config[protection_name].enabled = value
+        if protection_name in self.protections:
+            self.protections[protection_name].enabled = value
         else:
             raise ValueError(f"Protection {protection_name} not found.")
 
     async def check_all_exploits(self, **kwargs) -> bool:
         """Check for all exploits using the configured protection mechanisms.
-        It will only run the protection mechanism stored in the config if it is enabled.
+
+        A registered protection mechanism will only run if it is enabled.
 
         Args:
             **kwargs: Arbitrary keyword arguments that are accepted by different protection mechanisms.
 
         Returns:
-            bool: False if any of the protection mechanisms detect a violation, True otherwise.
+            bool: False if any of the protection mechanisms detects a violation (check failed),
+                True otherwise (check succeeded).
         """
-        for protection in self.config.values():
+        for protection in self.protections.values():
             if not await protection.check_for_violation(**kwargs):
                 return False
         return True
